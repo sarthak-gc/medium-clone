0fafb87da26f245dab72c7f878b12ca44a9c409e
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-04-15 07:48:41 +0545

    Page Not found

--- frontend/src/pages/extra/NotFound.tsx
+++ frontend/src/pages/extra/NotFound.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>PAGE NOT FOUND</h1>
-      <span>404</span>
-      <p>Out of nothing, something.</p>
-      <p>
-        You can find (just about) anything on Medium Rare — apparently even a
-        page that doesn’t exist.
+    <div className="max-w-xl sm:mx-auto  p-5 px-0 flex flex-col items-center justify-center gap-12 mt-12 text-[#757575] ">
+      <h1 className="mb-5">PAGE NOT FOUND</h1>
+      <span className="text-6xl">404</span>
+      <p className="text-3xl text-black">Out of nothing, something.</p>
+      <p className="text-center">
+        You can find (just about) anything on Medium — apparently even a page
+        that doesn&apos;t exist. Maybe these stories will take you somewhere
+        new?
       </p>
-      <p>Maybe these stories will take you somewhere new?</p>
+      <Link className="underline" to={"/home"}>
+        Home
+      </Link>
+
       {/* some Recommendations here */}
 
       <h1>Recommendations</h1>
