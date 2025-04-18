21abb8bf213d9de13b7f1dd80db9135491a1fe43
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-04-18 21:13:38 +0545

    frontend-deploy

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export const AXIOS = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
